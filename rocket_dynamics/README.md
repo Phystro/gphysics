771d23d57e0a2916d427641d05082aca0333b45c
root <phystroq@gmail.com>
2021-03-15 22:13:14 +0300

    full course rocket launch

---
+++ rocket_dynamics/README.md
@@ -0,0 +1,15 @@
+<p>
+<h2 align="center">Rocket Dynamics</h2>
+
+<hr>
+<br>
+
+<img align="center" src="images/single_stage_rocket_telemetry.png">
+
+<br>
+<hr>
+<br>
+
+<img align="center" src="images/single_stage_rocket_telemetry.svg">
+
+</p>
--- rocket_dynamics/rocket_sim.py
+++ rocket_dynamics/rocket_sim.py
@@ -4,57 +4,60 @@ NOTE: Observe SI units
 
 import numpy as np
 import matplotlib.pyplot as plt
+from time import sleep
 
 # constants
 PI = np.pi
-gravity = -9.81
-time_delta = 0.01
+gravity = 9.81
+time_delta = 0.1
+thrust = 800
 
-# initial conditions
-time_init = 0.0
-height_init = 0.0
-velocity_init = 0.0
-acceleration_init = 0.0
-
-mass_rocket = 5    # kg
-mass_fuel = 20      # kg
-thrust = 13000
-rate_consumption = 660  # kg/s
-
-class Rocket():
-    def __init__(self, mass_rocket, mass_fuel, thrust, rate_consumption):
-        self.gravity = gravity
-        self.height = height_init
-        self.velocity = velocity_init
-        self.mass_rocket = mass_rocket
-        self.mass_fuel = mass_fuel
-        self.mass_total = mass_rocket + mass_fuel
-        self.rate_consumption = rate_consumption
+class ObjectSpecs:
+    def __init__(self):
+        """Physical objects involved, their characteristics and properties/specifications"""
+        
+        # Rocket Specs
+        self.mass_rocket = 5    # [kg]
+        self.mass_fuel = 21     # [kg]
+        self.mass_total = self.mass_fuel + self.mass_rocket # [kg]
+        # self.thrust = 1300.0    # 
         self.thrust = thrust
-        self.acceleration = acceleration_init
+        self.rate_consumption = 0.0584     # fuel burn rate [kg/s]
+        self.effective_surface_radius = 0.1     # effective surface radius exposed to drag effects [m]
 
-    def get_thrust(self):
-        if self.mass_fuel <= 0:
-            thrust = 0.0
-        else:
-            thrust = 13000.0
-        return thrust
+class InitialConditions(ObjectSpecs):
+    def __init__(self):
+        ObjectSpecs.__init__(self)
 
-    def get_acceleration(self, h, v, t):
-        if self.mass_total >= self.mass_rocket:
-            g = self.get_gravity(h)
-            thrust = self.get_thrust()
-            mass = self.get_mass(t)
-            drag = self.get_drag(h, v)
+        # initial conditions
+        self.time_init = 0.0
+        self.height_init = 0.0000000001
+        self.velocity_init = 0.0
+        self.acceleration_init = 0.0
 
-            acc = ( thrust - mass*g - drag ) / mass # from Newton's second law
-            return acc
+        self.time = self.time_init
+        self.time_delta = time_delta
+
+        self.gravity = gravity
+        self.height = self.height_init
+        self.velocity = self.velocity_init
+        self.acceleration = self.acceleration_init
+
+        # arrays for collecting telemetry data
+        self.bins_time = []
+        self.bins_height = []
+        self.bins_velocity = []
+        self.bins_acceleration = []
+        self.bins_mass = []
 
 
-    def get_mass(self, time):
-        if self.mass_total >= self.mass_rocket:
-            self.mass_total = self.mass_rocket + ( self.mass_fuel - ( self.rate_consumption * time ) )
-        return self.mass_total
+class Physics:
+    def __init__(self):
+        pass
+    def get_gravity(self, h):
+        """Computes and returns value of gravitational acceleration. Gravitational acceleration varies with height from the Earth's surface."""
+        g = gravity / (1 + h/6371000)
+        return g
 
     def get_drag_coeff(self):
         """Assuming a constant drag coefficient.
@@ -62,59 +65,122 @@ class Rocket():
         drag_coefficient = 0.1  # for a well streamlined body
         return drag_coefficient
 
-    def get_drag(self, h, velocity):
-        effective_surface_radius = 0.1      # effective surface area exposed to drag effects
-        area_surface = 0.25 * PI * (effective_surface_radius**2)
-        drag_coeff = self.get_drag_coeff()
+
+class Simulation(InitialConditions):
+    def __init__(self):
+        InitialConditions.__init__(self)
+
+        while (self.height >= 0.0):
+
+            self.gravity = Physics().get_gravity(self.height)
+            self.get_mass_total(self.time)
+            self.get_thrust()
+            self.get_acceleration()
+            self.update_velocity()
+            self.update_height()
+
+            # print(self.time, "\t\t", self.mass_total, "\t\t", self.mass_fuel, "\t\t", self.mass_rocket, "\t\t", self.height)
+
+            # fill in telemetry data into arrays
+            self.bins_time.append(self.time)
+            self.bins_height.append(self.height)
+            self.bins_mass.append(self.mass_fuel)
+            self.bins_velocity.append(self.velocity)
+            self.bins_acceleration.append(self.acceleration)
+
+            self.inc_time()
+            # sleep(1)
+
+        print("TOUCHDOWN!")
+        self.plot_graphs()
         
+
+    def get_thrust(self):
+        if self.mass_fuel <= 0:
+            self.thrust = 0.0
+            self.mass_fuel = 0.0
+            self.mass_total = self.mass_fuel + self.mass_rocket
+            # print("[-] OUT-OF-FUEL! NO THRUST AVAILABLE!")
+        else:
+            self.thrust = thrust
+        return self.thrust
+
+    def get_mass_total(self, time):
+        if self.mass_total >= self.mass_rocket:
+            self.mass_fuel -= ( self.rate_consumption * time )
+            self.mass_total = self.mass_rocket + self.mass_fuel
+        return self.mass_total
+
+    def get_drag(self, h, velocity):
+        area_surface = 0.25 * PI * (self.effective_surface_radius**2)
+        drag_coeff = Physics().get_drag_coeff()
+
         # TODO: implement ISA atmosphere model
         atm_density = 1.225     # kg/m^3
 
         drag = 0.5 * drag_coeff * atm_density * area_surface * velocity**2
         return drag
 
-    def get_gravity(self, h):
-        """Computes and returns value of gravitational acceleration. Gravitational acceleration varies with height from the Earth's surface."""
-        g = gravity / (1 + h/6371000)
-        return g
+    def get_acceleration(self):
+        if self.mass_total >= self.mass_rocket:
+            # thrust = self.get_thrust()
+            # mass = self.get_mass_total(t)
+            drag = self.get_drag(self.height, self.velocity)
 
+            self.acceleration = ( self.thrust - self.mass_total*self.gravity - drag ) / self.mass_total # from Newton's second law
 
-class Simulation(Rocket):
-    def __init__(self, Rocket):
-        Rocket.__init__(self, mass_rocket, mass_fuel, thrust, rate_consumption)
+        return self.acceleration
 
-        self.time = time_init
-        self.time_delta = time_delta
+    def update_height(self):
+        self.height += self.velocity * self.time
 
-        bins_time = []
-        bins_height = []
-        bins_velocity = []
-        bins_velocity = []
-        bins_mass = []
+    def update_velocity(self):
+        self.velocity = self.velocity_init + ( self.acceleration * self.time )
 
-        while ( self.height >= 0 ):
-
-            self.get_acceleration(self.height, self.velocity, self.time)
-            self.update_velocity(self.acceleration)
-            self.update_height(self.height)
-            print(self.height)
-            self.inc_time()
-
-
-        
     def plot_graphs(self):
-        pass
+        fig, ( (gr_mass, gr_height), (gr_velocity, gr_acceleration) ) = plt.subplots(nrows=2, ncols=2, sharex=True)
 
-    def update_height(self, h):
-        h += self.velocity * self.time
-        return h
+        gr_mass.set_title("Rocket Fuel Mass (kg) vs Time (s)")
+        gr_mass.set_xlabel("Time (s)")
+        gr_mass.set_ylabel("Fuel Mass (kg)")
+        gr_mass.grid()
 
-    def update_velocity(self, a):
-        v = self.velocity + a * self.time
-        return v
+        gr_height.set_title("Height (m) vs Time (s)")
+        gr_height.set_xlabel("Time (s)")
+        gr_height.set_ylabel("Height (m)")
+        gr_height.grid()
+
+        gr_velocity.set_title("Velocity $(m/s)$ vs Time $(s)$")
+        gr_velocity.set_xlabel("Time $(s)$")
+        gr_velocity.set_ylabel("Velocity $(m/s)$")
+        gr_velocity.grid()
+
+        gr_acceleration.set_title("Acceleration $(m/s^2)$ vs Time $(s)$")
+        gr_acceleration.set_xlabel("Time $(s)$")
+        gr_acceleration.set_ylabel("Acceleration $(m/s^2)$")
+        gr_acceleration.grid()
+
+        gr_mass.plot(self.bins_time, self.bins_mass, label="mass")
+        gr_height.plot(self.bins_time, self.bins_height, label="Height")
+        gr_velocity.plot(self.bins_time, self.bins_velocity, label="Velocity")
+        gr_acceleration.plot(self.bins_time, self.bins_acceleration, label="Acceleration")
+
+        gr_mass.legend()
+        gr_height.legend()
+        gr_velocity.legend()
+        gr_acceleration.legend()
+
+        fig.suptitle("Single-Stage Rocket Simulation Telemetry Data")
+        fig.set_dpi(800)
+
+        plt.savefig("images/single_stage_rocket_telemetry.png", dpi=800)
+        plt.savefig("images/single_stage_rocket_telemetry.svg", dpi=800)
+
+        plt.show()
+        plt.close()
 
     def inc_time(self):
         self.time += self.time_delta
 
 
-Simulation(Rocket)
+Simulation()
